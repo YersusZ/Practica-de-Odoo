54c429f4c975eadc71f9ce2a31efb8f25a0c3e43
Daniel Toro soto <daniel.toro1@utp.edu.co>
2025-04-05 21:34:38 -0500

    videos 4 ,5

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -3,5 +3,10 @@
     "depends":[
         "base"
     ],
+    "data":[
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_property_menus.xml",
+    ],
     "application": True 
 }
